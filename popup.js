9db74ba6756441d79f9ab834877b84fd78cf49cb
Riley Shaw <rlyshw@rlyshw.com>
2016-07-04 20:56:35 -0400

    i dunno, just backing this up

--- popup.js
+++ popup.js
@@ -4,10 +4,12 @@
 
 var file;
 
-function upload(i, file){
+function upload_file(i, file){
+    console.log("uploading file");
     var ext = i.slice(i.length-4);
-    if(ext!=".png"||ext!="jpeg"||ext!=".jpg"||ext!=".gif")i+=".png"
-    var url = "http://localhost:3000/?name="+i;
+    //if(ext!=".png"||ext!="jpeg"||ext!=".jpg"||ext!=".gif"||ext[0]!=".")i+=".png"
+    if(ext[0]!=".")i+=".png";
+    var url = "http://128.4.12.128:1281/?name="+i;
     var oReq = new XMLHttpRequest();
     oReq.open("POST", url, true);
     oReq.onload = function(oEvent){
@@ -26,6 +28,25 @@ function upload(i, file){
     }
     oReq.send(file);
 }
+function upload_text(i){
+    console.log("uploading text")
+    var url = "http://128.4.12.128:1281/?name="+Date.now()+".txt";
+    var oReq = new XMLHttpRequest();
+    oReq.open("POST",url,true);
+    oReq.onloadend = function(data){
+        console.log(oReq.response);
+        var link = document.createElement("div");
+        link.innerHTML = oReq.response;
+        document.activeElement.appendChild(link);
+        var range = document.createRange();
+        range.selectNode(link);
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+    }
+    var formData = new FormData();
+    formData.append("text",i);
+    oReq.send(i);
+}
 
 window.addEventListener("paste", function (e) {
     for(var i=0;i<e.clipboardData.items.length;i++) {
@@ -35,7 +56,11 @@ window.addEventListener("paste", function (e) {
             var text = item.getAsString(function(i){
                 //console.log(i,file);
                 if(file){
-                    upload(i,file);
+                    console.log(file.size);
+                    upload_file(i.replace(/ /g,''),file);
+                }
+                else if(!file){
+                    upload_text(i);
                 }
                 var div = document.createElement("div");
                 div.innerHTML = i;
@@ -43,9 +68,11 @@ window.addEventListener("paste", function (e) {
             });
         }
         else if(item.kind=="file"){
-            file = item.getAsFile();
+            file = item.getAsFile(function(f){
+                console.log(f);
+            });
             var i = "a"+Date.now();
-            upload(i,file);
+            if(e.clipboardData.items.length==1)upload_file(i,file);
             var URLObj = window.URL || window.webkitURL;
             var source = URLObj.createObjectURL(file);
             
